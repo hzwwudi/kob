f2a7a7308eb72cfd4674c013e580f7606eca0183
hzwwudi@163.com <hzwwudi@163.com>
2023-02-02 18:09:16 +0800

    更新接口

--- backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
+++ backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
@@ -9,10 +9,10 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/pk/")
+@RequestMapping("/pk")
 public class BotInfoController {
 
-    @RequestMapping("getbotinfo/")
+    @RequestMapping("/getbotinfo")
     public Map<String, String> getBotInfo() {
         Map<String, String> bot1 = new HashMap<>();
         bot1.put("name", "apple");
